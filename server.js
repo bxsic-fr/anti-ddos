d8872573336a82d32d820fef3272aa12ac573065
Bxsic <65828028+bxsic-fr@users.noreply.github.com>
2021-02-26 14:58:32 +0100

    update 2
    Script updated with including the ddos protect code into a function

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const fs = require("fs-extra")
 
+
 //EDIT HERE
 let irq = 15; //Number of seconds of the requests interval.
 let maxrq = 10 //Number of requests accepted in the "irq" time interval.
+
 
 //DON'T TOUCH AFTER THAT
 const app = express();
@@ -12,20 +14,25 @@ irq = irq*1000;
 console.log("--> Cool ddos protection by Bxsic <--");
 
 app.get("/", (req, res) => {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  antiddos(req, res, irq, maxrq);
+  res.sendFile(__dirname + "/views/index.html")
+});
+
+function antiddos(request, response, interval, maxrequests) {
+  var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
   ip = ip.split(",");
   ip = ip['0'];
   fs.appendFileSync("listconnections.txt", ip + "+");
   let listip = fs.readFileSync("listconnections.txt", "utf-8").split('+');
   let nbrq = getnbrequests(listip, ip);
-  if(nbrq > maxrq){
-    res.redirect("http://" + ip);
-    console.log("❌" + ip + ", number of requests last " + irq/1000 + "s : " + nbrq + "/" + maxrq);
+  if(nbrq > maxrequests){
+    response.redirect("http://" + ip);
+    console.log("❌" + ip + ", number of requests last " + interval/1000 + "s : " + nbrq + "/" + maxrequests);
   } else {
-    res.sendFile(__dirname + "/views/index.html");
-    console.log("✅ " + ip + ", number of requests last " + irq/1000 + "s : " + nbrq + "/" + maxrq);
+    response.sendFile(__dirname + "/views/index.html");
+    console.log("✅ " + ip + ", number of requests last " + interval/1000 + "s : " + nbrq + "/" + maxrequests);
   }
-});
+}
 
 function getnbrequests(array, value) {
     var count = 0;
